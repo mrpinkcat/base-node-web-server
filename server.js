6ff1925407c8eb35551d008d95c645cea51ef8f1
Mr Pink <gatien.anizan@gmail.com>
2017-05-20 17:16:36 +0000

    First commit !

---
+++ server.js
@@ -0,0 +1,24 @@
+const _PORT = process.env.PORT || 3000;
+
+var express = require('express'), // express
+    app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io')(server);
+    
+server.listen(_PORT, function() {
+    console.log('Server up on :' + _PORT);
+});
+
+app.use('/css', express.static(__dirname + '/web/css/'));
+app.use('/js', express.static(__dirname + '/web/js/'));
+app.use('/img', express.static(__dirname + '/web/img/'));
+
+app.get('/', (req, res) => {
+    res.sendFile('web/index.html', {
+        root: __dirname
+    });
+});
+
+io.on('connection', function(socket) {
+    console.log('test')
+})
