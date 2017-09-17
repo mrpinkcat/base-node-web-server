dffdd7fbb247f18a13db8bdfcc2cadd2f562e038
Mr. Pink <gatien.anizan@gmail.com>
2017-09-17 19:30:47 +0200

    Missing semicolons | Tab space 2 | var to const

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const _PORT = process.env.PORT || 3000;
 
-var express = require('express'), // express
-    app = require('express')(),
-    server = require('http').createServer(app),
-    io = require('socket.io')(server);
+const express = require('express'), // express
+  app = require('express')(),
+  server = require('http').createServer(app),
+  io = require('socket.io')(server);
     
 server.listen(_PORT, function() {
-    console.log('Server up on :' + _PORT);
+  console.log('Server up on :' + _PORT);
 });
 
 app.use('/css', express.static(__dirname + '/web/css/'));
@@ -14,11 +14,11 @@ app.use('/js', express.static(__dirname + '/web/js/'));
 app.use('/img', express.static(__dirname + '/web/img/'));
 
 app.get('/', (req, res) => {
-    res.sendFile('web/index.html', {
-        root: __dirname
-    });
+  res.sendFile('web/index.html', {
+    root: __dirname
+  });
 });
 
 io.on('connection', function(socket) {
-    console.log('test')
-})
+  console.log('test');
+});
